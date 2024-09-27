ecd74685ce4faf59a7358c4ea2a70c6470b547a0
Jawahar Vinayagamurthy <jvinayagamurthy@atheerair.com>
2024-09-26 19:16:04 -0700

    Updates to portfolio section.

--- src/components/Sections/ItemOverlay.tsx
+++ src/components/Sections/ItemOverlay.tsx
@@ -12,7 +12,8 @@ const ItemOverlay: FC<ItemOverlayProps> = memo(({openModal, title}) => {
   return (
     <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 hover:bg-opacity-75 transition-opacity p-4">
       <h3 className="text-white font-semibold mb-2 text-center">{title}</h3> {/* Display title */}
-      <button onClick={openModal} className="text-white font-semibold bg-blue-500 rounded px-4 py-2 transition duration-300 hover:bg-blue-600">
+      <button className="text-white font-semibold bg-blue-500 rounded px-4 py-2 transition duration-300 hover:bg-blue-600"
+        onClick={openModal}>
         View Details
       </button>
     </div>
--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -1,43 +1,47 @@
 // Portfolio.tsx
-import React, {FC, memo, useState} from 'react';
+import React, {FC, memo, useState, useCallback} from 'react';
 import Image from 'next/image';
 import {portfolioItems as initialPortfolioItems, SectionId} from '../../data/data';
 import Section from '../Layout/Section';
 import PortfolioModal from './PortfolioModal';
 import ItemOverlay from './ItemOverlay';
 import {PortfolioItem} from '../../data/dataDef';
+import classNames from 'classnames';
 
 const Portfolio: FC = memo(() => {
   const [portfolioItems] = useState<PortfolioItem[]>(initialPortfolioItems);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentItem, setCurrentItem] = useState<PortfolioItem | null>(null);
 
-  const openModal = (item: PortfolioItem) => {
+  // Memoize the openModal function
+  const openModal = useCallback((item: PortfolioItem) => {
     setCurrentItem(item);
     setIsModalOpen(true);
-  };
+  }, []);
 
-  const closeModal = () => {
+  // Memoize the closeModal function
+  const closeModal = useCallback(() => {
     setIsModalOpen(false);
     setCurrentItem(null);
-  };
+  }, []);
 
   return (
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex flex-col gap-y-8">
         <h2 className="self-center text-xl font-bold text-white">Explore my project work</h2>
-        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {portfolioItems.map((item, index) => {
-            const {title, image} = item;
+            const { title, image } = item;
             return (
               <div className="relative" key={`${title}-${index}`}>
-                <Image
-                  alt={title}
-                  className="h-48 w-full object-cover rounded-lg" // Set height and width
-                  placeholder="blur"
-                  src={image}
-                />
-                <ItemOverlay item={item} openModal={() => openModal(item)} title={title} /> {/* Pass the title as well */}
+                <div
+                  className={classNames(
+                    'relative h-[250px] w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
+                  )}
+                >
+                  <Image alt={title} src={image} layout="fill" objectFit="cover" />
+                  <ItemOverlay item={item} openModal={() => openModal(item)} title={title} />
+                </div>
               </div>
             );
           })}
@@ -45,8 +49,8 @@ const Portfolio: FC = memo(() => {
         {currentItem && (
           <PortfolioModal
             isOpen={isModalOpen}
-            onClose={closeModal}
             item={currentItem}
+            onClose={closeModal} // Sorted alphabetically
           />
         )}
       </div>
@@ -56,3 +60,6 @@ const Portfolio: FC = memo(() => {
 
 Portfolio.displayName = 'Portfolio';
 export default Portfolio;
+
+//<ItemOverlay item={item} openModal={() => openModal(item)} title={title} /> {/* Pass the title as well */}
+
